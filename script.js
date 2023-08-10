e3cb953d8cdaff600078942cc12796570f36d077
chycherry <141926399+chycherry@users.noreply.github.com>
2023-08-10 21:38:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Assuming data.json contains an array of card objects with 'title' and 'content' properties
-
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
         const cardContainer = document.getElementById('cardContainer');
-        data.forEach(cardData => {
-            const card = document.createElement('div');
+        data.forEach((cardData, index) => {
+            const card = document.createElement('a'); // Change to 'a' element for linking
             card.classList.add('card');
+            card.href = `article-${index + 1}.html`; // Link to the corresponding article page
             card.innerHTML = `
-                <h2>${cardData.title}</h2>
-                <p>${cardData.content}</p>
+                <img src="${cardData.image}" alt="${cardData.title}" class="card-image">
+                <div class="card-content">
+                    <h2>${cardData.title}</h2>
+                    <p>${cardData.description}</p>
+                </div>
             `;
             cardContainer.appendChild(card);
         });
